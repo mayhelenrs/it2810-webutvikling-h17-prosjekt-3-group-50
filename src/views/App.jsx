bfbaeb144677dda7e842105391d726bea0535dba
Westermoen <jakob@westermoen.com>
2017-09-25 14:44:28 +0200

    initial commit

---
+++ src/views/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../assets/styles/App.css';
+
+export class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h2 className="HeaderText">Welcome to iPIM</h2>
+        <p className="App-intro">
+          Middag? yes please
+        </p>
+      </div>
+    );
+  }
+}
