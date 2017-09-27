7125a49b7d06486555a603d0971bc064f83f6508
mariusaarsnes <marius.aarsnes@gmail.com>
2017-09-27 18:28:51 +0200

    added some test elements to show how things will work, this should be removed lated

--- src/views/frontpage.jsx
+++ src/views/frontpage.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import '../assets/styles/App.css';
-import {Header} from '../components/Header.jsx'
-//import {NoteHolder} from '../components/NoteHolder.jsx'
+import {Navbar} from '../components/Navbar.jsx'
 
 export class Frontpage extends React.Component {
     render() {
         return (
-            <Header navbarLocation="DASHBOARD"
-                    navbarLinks={<li><a href={"#"}>Notes</a></li>}
+
+            <Navbar navbarLocation={"DASHBOARD"}
+
+                    /*Remove this later, this is just to help with styling and an example */
+                    navbarLinks={
+                        <ul className={"Navbar-links"}>
+                            <li>
+                                <a><h2>Link 1</h2></a>
+                            </li>
+                            <li>
+                                <a><h2>Link 2</h2></a>
+                            </li>
+                        </ul>
+                    }
 
             />
         );
