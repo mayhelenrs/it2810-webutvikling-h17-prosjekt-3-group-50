3fc956482250c6b0868418a3bdd4c5fa224b8b9a
mariusaarsnes <marius.aarsnes@gmail.com>
2017-09-27 18:28:19 +0200

    changed header.jsx to Navbar.jsx to be consistent, also finished adding necessery elements and some comments

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,19 @@ import React from 'react';
 import '../assets/styles/Component.css';
 
 
-export class Header extends React.Component {
+export class Navbar extends React.Component {
     render() {
         return (
             <div className="Navbar">
                 <img alt={"icon of a speedometer"} src={require("../assets/images/navbar_lefticon.png")}/>
+
+                {/*navbarLocation: Name of the page the user is on. */}
                 <h1>{this.props.navbarLocation}</h1>
+
+                {/* navbarLinks: Send in a <ul> with <li> child-elements containing <a> and <h2> tags.
+                    This way we have all the pages, one click away,
+                    instead of having to go back to the main page.
+                 */}
                 {this.props.navbarLinks}
             </div>
         );
