04a2cf5c9c17a88dbf38822d03851b13a61c0972
mariusaarsnes <marius.aarsnes@gmail.com>
2017-09-29 16:20:16 +0200

    Tiles now have correct icons

--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -4,10 +4,10 @@ import '../assets/styles/Component.css';
 export class Tile extends React.Component {
     render() {
         return(
-            <div className={"Frontpage-tile"}>
-                <img alt={"Icon for tile"} src={required(this.props.tileImgSrc)}/>
+            <div className={"Tile"}>
+                <p><img  src={this.props.tileIcon} alt={"Icon for tile"} className={"Tile-img"}/></p>
                 <h3>{this.props.tileTitle}</h3>
-                <button>Enter <img alt={"Arrow pointing to the right"} src={required("djeie")}/></button>
+                <button>Enter <img alt={"Arrow pointing to the right"} /*src={require("djeie")}*//></button>
             </div>
         );
     }
