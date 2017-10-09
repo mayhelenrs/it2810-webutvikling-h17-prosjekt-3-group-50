14c540a4143d47d167a3bbc2c1fc19f067733f50
mariusaarsnes <marius.aarsnes@gmail.com>
2017-10-09 15:17:12 +0200

    made new folder for webiste and app and moved everything for website into right folder

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-export * from './Navbar.jsx';
-export * from './Notes/Note.jsx';
-export * from './Pagetext.jsx';
-export * from './Tile.jsx';
-export * from './TileGrid.jsx';
-export * from './ToDo.jsx';
