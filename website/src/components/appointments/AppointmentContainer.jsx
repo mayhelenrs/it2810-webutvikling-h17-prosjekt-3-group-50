2f6ec0ec737f156fe7f9202af5632896c2afe485
Westermoen <jakob@westermoen.com>
2017-10-11 15:29:20 +0200

    Beautiful indent

--- website/src/components/appointments/AppointmentContainer.jsx
+++ website/src/components/appointments/AppointmentContainer.jsx
@@ -1,105 +1,86 @@
 import React from 'react';
-import { AppointmentItem } from './appointmentItem.jsx';
+import {AppointmentItem} from './appointmentItem.jsx';
 import '../../assets/styles/appointment.css';
 
 export class AppointmentContainer extends React.Component {
-  constructor(props) {
-    super(props)
-    let appointmentList = [
-    ]
-    this.state = {
-      list: appointmentList,
-      displayList: appointmentList,
-      desc: '',
-      time:'',
-      date:''
+    constructor(props) {
+        super(props)
+        let appointmentList = []
+        this.state = {
+            list: appointmentList,
+            displayList: appointmentList,
+            desc: '',
+            time: '',
+            date: ''
+        }
+        this.formSubmit = this.formSubmit.bind(this)
+        this.filter = this.filter.bind(this)
     }
-    this.formSubmit = this.formSubmit.bind(this)
-    this.filter = this.filter.bind(this)
-  }
 
-  handleChange(event){
-    this.setState({
-      [event.target.name]: event.target.value
-    })
-  }
-
-  filter(){
-    const appointList = this.props.selectedColor() === undefined
-                            ? this.state.list.map((item) => item)
-                            : this.state.list.filter((item) => item.props.color === this.props.selectedColor());
-    this.setState((prevState) => {
-      return {...prevState, displayList: appointList}
-    })
-  }
-
-  getColor(){
-    const color = this.props.selectedColor()
-    return color === undefined ? '#016D91':color
-  }
-
-  formSubmit(e){
-    if(this.state.desc.length > 0 &&
-      this.state.time.length > 0 &&
-      this.state.date.length > 0){
-      const newList = this.state.list.slice()
-      const index = newList.length
-      const newAppointment = <AppointmentItem
-        description={this.state.desc}
-        time={this.state.time}
-        date={this.state.date}
-        key={index}
-        color={this.getColor()}
-        />
-      newList.push(newAppointment)
-      this.setState({
-        list: newList
-      }, () => this.filter())
-
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
     }
-    e.preventDefault()
-  }
 
-  render() {
-    return (
-      <div>
-        <ul className="appointmentCards">
-          {this.state.displayList}
-        </ul>
-        <form onSubmit= {this.formSubmit} className="formAppointment">
-          <ul className="formList">
-            <li>
-              <input type="text"
-                name="desc"
-                value={this.state.desc}
-                onChange={this.handleChange.bind(this)}
-                placeholder="Insert description"
-              />
-            </li>
-            <li>
-              <input type="text"
-                name="time"
-                value={this.state.time}
-                onChange={this.handleChange.bind(this)}
-                placeholder="Insert time"
-              />
-            </li>
-            <li>
-              <input type="text"
-                name="date"
-                value={this.state.date}
-                onChange={this.handleChange.bind(this)}
-                placeholder="Insert date"
-              />
-            </li>
-            <li>
-              <button onSubmit={this.formSubmit} id="formButton">
-                Add
-              </button>
-            </li>
-          </ul>
-        </form>
-      </div>
-    );
-  }
+    filter() {
+        const appointList = this.props.selectedColor() === undefined
+            ? this.state.list.map((item) => item)
+            : this.state.list.filter((item) => item.props.color === this.props.selectedColor());
+        this.setState((prevState) => {
+            return {
+                ...prevState,
+                displayList: appointList
+            }
+        })
+    }
+
+    getColor() {
+        const color = this.props.selectedColor()
+        return color === undefined
+            ? '#016D91'
+            : color
+    }
+
+    formSubmit(e) {
+        if (this.state.desc.length > 0 && this.state.time.length > 0 && this.state.date.length > 0) {
+            const newList = this.state.list.slice()
+            const index = newList.length
+            const newAppointment = <AppointmentItem description={this.state.desc} time={this.state.time} date={this.state.date} key={index} color={this.getColor()}/>
+            newList.push(newAppointment)
+            this.setState({
+                list: newList
+            }, () => this.filter())
+
+        }
+        e.preventDefault()
+    }
+
+    render() {
+        return (
+            <div>
+                <ul className="appointmentCards">
+                    {this.state.displayList}
+                </ul>
+                <form onSubmit={this.formSubmit} className="formAppointment">
+                    <ul className="formList">
+                        <li>
+                            <input type="text" className="appointmentInput" name="desc" value={this.state.desc} onChange={this.handleChange.bind(this)} placeholder="Insert description"/>
+                        </li>
+                        <li>
+                            <input type="text" className="appointmentInput" name="time" value={this.state.time} onChange={this.handleChange.bind(this)} placeholder="Insert time"/>
+                        </li>
+                        <li>
+                            <input type="text" className="appointmentInput" name="date" value={this.state.date} onChange={this.handleChange.bind(this)} placeholder="Insert date"/>
+                        </li>
+                        <li>
+                            <button onSubmit={this.formSubmit} id="formButton">
+                                Add
+                            </button>
+                        </li>
+                    </ul>
+                </form>
+            </div>
+        );
+    }
 }
--- website/src/components/appointments/appointmentItem.jsx
+++ website/src/components/appointments/appointmentItem.jsx
@@ -2,22 +2,24 @@ import React from 'react';
 import '../../assets/styles/appointment.css';
 
 export class AppointmentItem extends React.Component {
-  render(){
-    return(
-      <li className="tile-wrapper" key={this.props.index}>
-        <div className="color-tile" style={{backgroundColor: ''+this.props.color , borderColor: ''+this.props.color}}></div>
-        <div className="titleText">
-          <p>{this.props.description}</p>
-        </div>
-        <div className="dateText">
-          <p>{this.props.time}</p>
-          <p>{this.props.date}</p>
-        </div>
-        <div className="deleteButton">
-          <img src={require("../../assets/images/close_red.png")} alt={"Delete button"}/>
-        </div>
-      </li>
-
-    );
-  }
+    render() {
+        return (
+            <li className="tile-wrapper" key={this.props.index}>
+                <div className="color-tile" style={{
+                    backgroundColor: '' + this.props.color,
+                    borderColor: '' + this.props.color
+                }}></div>
+                <div className="titleText">
+                    <p>{this.props.description}</p>
+                </div>
+                <div className="dateText">
+                    <p>{this.props.time}</p>
+                    <p>{this.props.date}</p>
+                </div>
+                <div className="deleteButton">
+                    <img src={require("../../assets/images/close_red.png")} alt={"Delete button"}/>
+                </div>
+            </li>
+        );
+    }
 }
