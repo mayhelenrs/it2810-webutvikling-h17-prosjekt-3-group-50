3664cbf3cfbc145848e6d2dad4ab17f2588df5b8
Westermoen <jakob@westermoen.com>
2017-10-13 13:32:28 +0200

    something?

---
+++ app/views/frontpage.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+import Navbar from '../components/Navbar';
+import TileGrid from "../components/TileGrid";
+
+export default class Frontpage extends React.Component {
+    static navigationOptions = {
+        title: 'DASHBOARD',
+        headerTitleStyle: {
+            color: 'black',
+            fontFamily: 'IntroRust'
+        },
+        headerStyle: {
+            backgroundColor: '#00ACCE',
+        }
+    };
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={styles.content}>
+                    <TileGrid style={styles.tiles} navigation={this.props.navigation}/>
+                </View>
+            </View>
+        );
+    }
+};
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 'auto',
+        flex: 1,
+        backgroundColor: '#fff',
+        flexDirection: 'column'
+    },
+    content: {
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+});
