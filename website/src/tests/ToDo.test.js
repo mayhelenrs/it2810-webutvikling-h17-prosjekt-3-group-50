eaac44b7b06a9e7a0d77ea622e9665d06272380e
martinlunde <martlun@stud.ntnu.no>
2017-10-16 17:12:04 +0200

    added Enzyme and wrote some more tests

--- website/src/tests/ToDo.test.js
+++ website/src/tests/ToDo.test.js
@@ -3,10 +3,15 @@ import localStorage from './localStorageMock';
 
 import React from 'react';
 import renderer from 'react-test-renderer';
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import {shallow, mount, render} from 'enzyme';
 import {ToDoView} from "../views/ToDoView";
 import {ToDo} from "../components/Todo/ToDo";
 import {ToDoItem} from "../components/Todo/ToDoItem";
 import {CategoryItem} from "../components/Todo/Items";
+
+Enzyme.configure({ adapter: new Adapter() });
 
 /* Main function for testing the: */
 /* todo application */
@@ -18,9 +23,9 @@ describe('ToDo Overview', () => {
     });
 });
 
-/* Test function for testing the: */
+/* Test function for testing the rendering of: */
 /* todo components */
-describe('ToDo Components', () => {
+describe('ToDo Component Renders', () => {
     it('renders the Category Component', () => {
        const instance = renderer.create(<ToDo/>);
        const tree = instance.toJSON();
@@ -43,5 +48,52 @@ describe('ToDo Components', () => {
         const instance = renderer.create(<ToDoItem/>);
         const tree = instance.toJSON();
         expect(tree).toMatchSnapshot();
+    });
+});
+
+// Test instatiation of CategoryItem properties.
+describe('CategoryItem', () => {
+    it('instantiates properties properly ', () => {
+        const wrapper = shallow(
+            <CategoryItem className="items"
+                           value={"Home"}
+                           color={"rgb(43, 186, 178)"}
+                           key={3}
+                           onClick={() => this.handleClicks(3)}/>
+        );
+        let current = wrapper.instance()['_currentElement'];
+        expect(current.key).toEqual("3");
+        expect(current.props.value).toEqual("Home");
+        expect(current.props.color).toEqual("rgb(43, 186, 178)");
+    });
+});
+
+// Test instatiation of TodoItem and its properties/State.
+describe('TodoItem', () => {
+    it('instantiates properties and state properly', () => {
+        const todo = shallow(<ToDo/>);
+        const wrapper = shallow(
+            <ToDoItem className="items"
+                      value={todo}
+                      key={2}
+                      color={"rgb(43, 186, 178)"}
+                      onClick={() => this.handleClicks(index)}/>
+        );
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    //Tests if the handleClick function changes the state from false to true
+    it('handleClick changes state', ()=> {
+        const todo = shallow(<ToDo/>);
+        const wrapper = shallow(
+            <ToDoItem className="items"
+                      value={todo}
+                      key={2}
+                      color={"rgb(43, 186, 178)"}
+                      onClick={() => this.handleClicks(index)}/>
+        );
+        expect(wrapper.instance()['_instance'].state).toEqual({"checked": false});
+        wrapper.instance()['_instance'].handleClick();
+        expect(wrapper.instance()['_instance'].state).toEqual({"checked": true});
     });
 });
