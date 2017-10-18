8ba3089f9ecd4a19326ad0b86f52e767d2920a45
danielromanich <daniel@romanich.com>
2017-10-18 16:27:46 +0200

    Fix todo bug in website

--- website/src/components/todo/ToDo.jsx
+++ website/src/components/todo/ToDo.jsx
@@ -11,11 +11,11 @@ export class ToDo extends React.Component {
         this.state = {
             filter: '',
             value: '',
-            current_color: '',
-            color_data: colors_todo,
-            data: todos,
-            displayed_data: todos,
-            displayed_colors: colors_todo,
+            currentColor: '',
+            colorData: [],
+            data: [],
+            displayedData: [],
+            displayedColors: [],
             indexList: [],
         };
         this.handleChange = this.handleChange.bind(this);
@@ -28,9 +28,12 @@ export class ToDo extends React.Component {
         LocalStorage.load(this.getSaveName(), (data) => {
             let colorData = data[0];
             let todoData = data[1];
-            this.setState({
-                colorData: colorData,
-                data: todoData,
+            this.setState((prevState) => {
+                return {
+                ...prevState,
+                    colorData: colorData,
+                    data: todoData,
+                };
             }, () => this.filter());
         }, [this.state.colorData, this.state.data]);
     }
@@ -43,18 +46,18 @@ export class ToDo extends React.Component {
         if (color === undefined) {
             color = "#016D91";
         }
-        this.setState({current_color: color});
-        let displayed_colors = this.props.selectedColor() === undefined ? this.state.color_data :
-            this.state.color_data.filter((color, index) => color === this.props.selectedColor());
-        let displayed_data = this.props.selectedColor() === undefined ? this.state.data :
-            this.state.data.filter((todo, index) => this.state.color_data[index] === this.props.selectedColor());
+        this.setState({currentColor: color});
+        let displayedColors = this.props.selectedColor() === undefined ? this.state.colorData :
+            this.state.colorData.filter((color, index) => color === this.props.selectedColor());
+        let displayedData = this.props.selectedColor() === undefined ? this.state.data :
+            this.state.data.filter((todo, index) => this.state.colorData[index] === this.props.selectedColor());
         let indexList = [];
         this.state.colorData.forEach((color, index)=> {
             if(this.props.selectedColor() === undefined || this.props.selectedColor() === color) {
                 indexList.push(index);
             }
         });
-        this.setState({displayedColors: displayed_colors, displayedData: displayed_data, indexList: indexList});
+        this.setState({displayedColors: displayedColors, displayedData: displayedData, indexList: indexList});
     }
 
     //Fires everytime the input text field change, updates value
@@ -97,8 +100,8 @@ export class ToDo extends React.Component {
 
     //Render the child elements from ToDoItem, sends down the displayed todoData and colors.
     renderToDoItems() {
-        return this.state.displayed_data.map((todo, index) =>
-            <ToDoItem className="items" value={todo} key={this.state.indexList[index]} color={this.state.displayed_colors[index]}
+        return this.state.displayedData.map((todo, index) =>
+            <ToDoItem className="items" value={todo} key={this.state.indexList[index]} color={this.state.displayedColors[index]}
                       onClick={() => this.handleClicks(this.state.indexList[index])}/>
         );
     }
