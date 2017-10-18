9bc393e802184b1f2d21724423950a1249997396
danielromanich <daniel@romanich.com>
2017-10-18 14:25:43 +0200

    Rewrite appointment to use our LocalStore class

--- website/src/components/appointments/AppointmentItem.jsx
+++ website/src/components/appointments/AppointmentItem.jsx
@@ -1,69 +1,45 @@
 import React from 'react';
 import '../../assets/styles/appointment.css';
+import {LocalStorage} from "../../service/LocalStorage";
 
 export class AppointmentItem extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            description: this.props.description,
-            color: this.props.color,
-            time: this.props.time,
-            date: this.props.date,
-            checked: false
-        }
-    }
 
-    handleClick() {
-        this.setState({checked: !this.state.checked})
-    }
     componentDidUpdate() {
-        this.save();
+        LocalStorage.save(this.getSaveName(), this.state);
     }
 
     componentDidMount() {
-        const data = this.load();
-        if (data !== null) {
-            this.setState(prevState => {
-                return {...prevState, data}
-            });
-        }
-        this.save();
-    }
-
-    save() {
-        localStorage.setItem("Appointment" + this.props.id, JSON.stringify(this.state));
-    }
-
-    load() {
-        return JSON.parse(localStorage.getItem("Appointment" + this.props.id));
+        LocalStorage.loadToState(this.getSaveName(), this);
     }
 
     removeAppointment() {
         this.props.handleRemove(this);
-        localStorage.removeItem("Appointment" + this.props.id)
+    }
+
+    getSaveName() {
+        return "Appointment" + this.props.id;
     }
     render() {
-        const text = this.state.checked;
         return (
             <div className="tile-wrapper" key={this.props.index}>
                 <div className="color-tile" style={{
-                    backgroundColor: '' + this.state.color,
-                    borderColor: '' + this.state.color
+                    backgroundColor: '' + this.props.color,
+                    borderColor: '' + this.props.color
                 }}></div>
                 <div className="textComponents">
                     <div className="titleText">
-                        <p>{this.state.description}</p>
+                        <p>{this.props.description}</p>
                     </div>
                     <div className="timeText">
-                        <p>{this.state.time}</p>
+                        <p>{this.props.time}</p>
                     </div>
                     <div className="dateText">
-                        <p>{this.state.date}</p>
+                        <p>{this.props.date}</p>
                     </div>
                 </div>
                 <div className="deleteButtonWrap">
-                    <input type="button" className="deleteButton" label={text}
-                        onClick={() => {this.removeAppointment()}} onChange={() => this.handleClick()}/>
+                    <input type="button" className="deleteButton"
+                        onClick={() => {this.removeAppointment()}} />
                 </div>
             </div>
         );
