8edf714a62f1a88784fac8fe1365dd585c281343
mariusaarsnes <marius.aarsnes@gmail.com>
2017-10-18 17:41:44 +0200

    fix merge conflict

--- website/src/components/index.js
+++ website/src/components/index.js
@@ -3,11 +3,8 @@ export * from './notes/Note.jsx';
 export * from './Pagetext.jsx';
 export * from './Tile.jsx';
 export * from './TileGrid.jsx';
-<<<<<<< HEAD
-export * from './Todo/ToDo.jsx';
+export * from './todo/ToDo.jsx';
 
 // This file is simply used to make imports easier. instead of having to import components for different files, we
 // import them from this file.
-=======
-export * from './todo/ToDo.jsx';
->>>>>>> 4358d9405333a48b40464943e57603e8f255ae0d
+
