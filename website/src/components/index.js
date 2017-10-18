8f318f3e5128b859d051b2bef6cf34fb2f56e444
danielromanich <daniel@romanich.com>
2017-10-18 15:32:50 +0200

    Fix todo folder link

--- website/src/components/index.js
+++ website/src/components/index.js
@@ -3,4 +3,4 @@ export * from './notes/Note.jsx';
 export * from './Pagetext.jsx';
 export * from './Tile.jsx';
 export * from './TileGrid.jsx';
-export * from './Todo/ToDo.jsx';
+export * from './todo/ToDo.jsx';
