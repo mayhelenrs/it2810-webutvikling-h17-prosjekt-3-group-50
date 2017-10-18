703bbf4b3b9635813d6b9b691c816aa75e2647c2
Westermoen <jakob@westermoen.com>
2017-10-18 13:34:17 +0200

    fix async Storage

--- app/components/appointments/AppointmentItem.js
+++ app/components/appointments/AppointmentItem.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, AsyncStorage} from 'react-native';
 import {Button} from 'react-native-elements';
 
 export class AppointmentItem extends React.Component {
@@ -10,20 +10,58 @@ export class AppointmentItem extends React.Component {
             color: this.props.color,
             time: this.props.time,
             date: this.props.date,
-            checked: false
         }
+        //checks if appointment exists, if not it will save it
+        AsyncStorage.getItem("Appointment" + this.props.id).then((data) => {
+            if (data === null) {
+                this.save();
+                this.load();
+            }
+        }).catch((ex) => {
+
+        });
+    }
+    //Loads the component right before its mounted
+    componentWillMount() {
+        this.load();
     }
 
     handleClick() {
         this.setState({checked: !this.state.checked})
     }
 
+    //removes appointment from storage
     removeAppointment() {
         this.props.handleRemove(this);
+        AsyncStorage.removeItem("Appointment" + this.props.id);
+    }
+
+    save() {
+        try {
+            //We create this object to make sure it saves as unselected
+            //This way it won't be open next time we reload the note
+            let state = Object.assign({}, this.state);
+            AsyncStorage.setItem("Appointment" + this.props.id, JSON.stringify(state));
+        } catch (error) {
+
+        }
+    }
+
+    //Function for loading the state of the enote
+    load() {
+        AsyncStorage.getItem("Appointment" + this.props.id).then((data) => {
+            if (data !== null) {
+                data = JSON.parse(data);
+                this.setState(() => {
+                    return data;
+                }, () => this.filter());
+            }
+        }).catch((ex) => {
+
+        });
     }
 
     render() {
-        const text = this.state.checked;
         return (
             <View style={[{
                 backgroundColor: '' + this.state.color,
